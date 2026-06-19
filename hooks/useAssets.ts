07de9aaed217ebfa9656ad6053e537b72054323d
Jinu <jbljw02@naver.com>
2026-06-19 18:15:34 +0900

    feat: useAuthUserId 훅 구현

--- hooks/useAssets.ts
+++ hooks/useAssets.ts
@@ -1,10 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { assetRepository } from '@/repositories/asset.repository';
 import { Asset } from '@/types/domain/asset';
+import { useAuthUserId } from '@/hooks/useAuthUserId';
+import { isNotNil } from '@/utils/validators';
 
 export const useAssets = () => {
+  const { data: userId } = useAuthUserId();
+
   return useQuery<Asset[], Error>({
-    queryKey: ['assets'],
+    queryKey: ['assets', userId],
     queryFn: () => assetRepository.getAssets(),
+    enabled: isNotNil(userId),
   });
 };
